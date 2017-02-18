5479a37565d8c82242548d12aa3a16f93723d093
ClarkLindsay <clindsay6821@volmail.jalc.edu>
2017-02-19 02:15:51 +1200

    modified color to be final

--- edu/jalc/trumpet/material/Material.java
+++ edu/jalc/trumpet/material/Material.java
@@ -5,21 +5,14 @@ import edu.jalc.trumpet.materialProperties.etching.Etching;
 
 public class Material {
 
-   private Color trumpetColor;
+   private final Color trumpetColor;
    private Etching trumpetEtching;
       
    public Material(Color trumpetColor, Etching trumpetEtching) {
       this.trumpetColor = trumpetColor;
       this.trumpetEtching = trumpetEtching;
    }
-   
-   void setColor(Color color) throws Exception {
-      if(color == null) 
-         throw new Exception("Color cannot be null");
       
-      trumpetColor = color;
-   }
-   
    void setEtching(Etching trumpetEtching){ 
       this.trumpetEtching = trumpetEtching;
    }
